536960a9e2c89d35a531e1c357b0a423a1cf18ae
Adisor <adisorpl@gmail.com>
2025-01-03 19:43:31 +0100

    Graph

---
+++ graph.js
@@ -0,0 +1,101 @@
+class Graph {
+    // Variables
+    #_Items = new Map();
+
+    // Constructor
+    constructor() {}
+
+    // Methods
+    #addEdge(nodeA, nodeB) {
+        if (!this.#_Items.has(nodeA)) {
+            this.#_Items.set(nodeA, new Set());
+        }
+        this.#_Items.get(nodeA).add(nodeB);
+
+        if (!this.#_Items.has(nodeB)) {
+            this.#_Items.set(nodeB, new Set());
+        }
+        this.#_Items.get(nodeB).add(nodeA);  // Dla grafu nieskierowanego
+    }
+
+    addNode(node) {
+        if (!this.#_Items.has(node)) {
+            this.#_Items.set(node, new Set());
+        }
+    }
+
+    addNodes(nodes = []) {
+        for (let i = 0; i < nodes.length; i++) {
+            for (let j = 0; j < nodes.length; j++) {
+                if (i !== j) {
+                    this.#addEdge(nodes[i], nodes[j]);
+                }
+            }
+        }
+    }
+
+    clear() {
+        this.#_Items.clear();
+    }
+
+    // Getters
+    get length() {
+        return this.#_Items.size;
+    }
+
+    get isEmpty() {
+        return this.#_Items.size === 0;
+    }
+
+    getEdges(node) {
+        return this.#_Items.has(node) ? [...this.#_Items.get(node)] : undefined;
+    }
+
+    contains(node) {
+        return this.#_Items.has(node);
+    }
+
+    isOrphan(node) {
+        return this.contains(node) && this.#_Items.get(node).size === 0;
+    }
+
+    isConnected(nodeA, nodeB) {
+        return this.#_Items.has(nodeA) && this.#_Items.get(nodeA).has(nodeB);
+    }
+
+    // Nowe metody dotyczące grup
+
+    #dfs(node, visited, component) {
+        visited.add(node);
+        component.push(node);
+        for (let neighbor of this.#_Items.get(node)) {
+            if (!visited.has(neighbor)) {
+                this.#dfs(neighbor, visited, component);
+            }
+        }
+    }
+
+    findConnectedComponents() {
+        let visited = new Set();
+        let components = [];
+
+        for (let node of this.#_Items.keys()) {
+            if (!visited.has(node)) {
+                let component = [];
+                this.#dfs(node, visited, component);
+                components.push(component);
+            }
+        }
+
+        return components;
+    }
+
+    getComponent(node) {
+        for (let component of this.findConnectedComponents()) {
+            if (component.includes(node)) {
+                return component;
+            }
+        }
+        return undefined;  // Węzeł nie istnieje w grafie
+    }
+}
